45b9bb08e697a0131341c6f7cbe2d10ea8259332
lucasmoncada08 <lucas.moncada08@gmail.com>
2022-07-04 20:30:38 -0400

    Set up eslint and dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-export default function Home() {
+const Home = () => {
+  console.log('here');
+
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>
+      <h1 className="text-3xl font-bold underline">
         Hello World
       </h1>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
