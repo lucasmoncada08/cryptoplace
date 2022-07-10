96c1983b33b94167703888c0e41849b28ef25a41
lucasmoncada08 <lucas.moncada08@gmail.com>
2022-07-09 21:28:37 -0400

    Added complete Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 const Home = () => (
   <div>
-    <h1 className="text-3xl font-bold underline">
+    {/* <h1 className="text-3xl font-bold underline">
       Hello World
-    </h1>
+    </h1> */}
   </div>
 );
 
