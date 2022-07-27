15190f1493fbe50c4327787f10325f01ef34341d
lucasmoncada08 <lucas.moncada08@gmail.com>
2022-07-26 23:25:17 -0400

    Added home screen banner and template for banners

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import Head from 'next/head';
+import { Banner } from '../components';
 
 const Home = () => (
-  <Head>
-    <title>Cryptoplace</title>
-    <meta name="description" content="nft marketplace application using ethereum" />
-  </Head>
+  <>
+    <Head>
+      <title>Cryptoplace</title>
+      <meta name="description" content="nft marketplace application using ethereum" />
+    </Head>
+    <div className="flex justify-center sm:px-4 p-12">
+      <div className="w-full minmd:w-4/5">
+        <Banner
+          name="Discover, collect, and sell spectacular NFTs"
+          parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+          childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
+        />
+      </div>
+    </div>
+  </>
 );
 
 export default Home;
