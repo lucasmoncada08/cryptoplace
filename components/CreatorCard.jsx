5421d01a9b0533b121f79186a1266e8e31bfc94a
lucasmoncada08 <lucas.moncada08@gmail.com>
2022-07-27 21:48:22 -0400

    Added images to creator card

--- components/CreatorCard.jsx
+++ components/CreatorCard.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import Image from 'next/image';
 
-const CreatorCard = () => (
-  <div>CreatorCard</div>
+import Images from '../assets';
+
+const CreatorCard = ({ rank, creatorImage, creatorName, creatorEths }) => (
+  <div className="min-w-190 minlg:min-w-240 dark:bg-nft-black-3 bg-white border dark:border-nft-black-3 border-nft-gray-1 rounded-3xl flex flex-column p-4 m-4">
+    <div>
+      <p>{rank}</p>
+    </div>
+    <div className="my-2 flex justify-center">
+      <div className="relative w-20 h-20 minlg:w-28 minlg:h-28">
+        <Image
+          src={creatorImage}
+          layout="fill"
+          objectFit="cover"
+          alt="creatorName"
+        />
+      </div>
+    </div>
+  </div>
 );
 
 export default CreatorCard;
