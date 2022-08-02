dfcde394d2a775efc645571ce14a9f631c1d6488
lucasmoncada08 <lucas.moncada08@gmail.com>
2022-08-02 18:10:26 -0400

    Finished FE portion of the create-nft upload

--- pages/create-nft.js
+++ pages/create-nft.js
@@ -23,8 +23,12 @@ const CreateNFT = () => {
   const [fileUrl, setFileUrl] = useState(null);
 
   const fileStyle = useMemo(() => (
-    ''
-  ), []);
+    `dark:bg-nft-black-1 bg-white border dark:border-nft-gray-2 flex flex-col items-center p-5 rounded-sm border-dashed 
+    ${isDragActive && ' border-file-active'}
+    ${isDragAccept && ' border-file-accept'}
+    ${isDragReject && ' border-file-reject'}
+    `
+  ), [isDragActive, isDragAccept, isDragReject]);
 
   return (
     <div className="flex justify-center p-12 sm:px-4">
@@ -33,7 +37,31 @@ const CreateNFT = () => {
         <div className="mt-16">
           <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl">Upload File</p>
           <div className="mt-4">
-            <div className={fileStyle} />
+            <div {...getRootProps()} className={fileStyle}>
+              <input {...getInputProps()} />
+              <div className="flexCenter flex-col text-center">
+                <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl">JPG, PNG, GIF, SVG, and WEBM. Max 100MB</p>
+                <div className="my-12 w-full flex justify-center">
+                  <Image
+                    src={images.upload}
+                    width={100}
+                    height={100}
+                    objectFit="contain"
+                    alt="upload image"
+                    className={theme === 'light' ? 'filter invert' : undefined}
+                  />
+                </div>
+                <div>
+                  <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm">Drag and Drop Files</p>
+                  <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm mt-2">Or Browse Media on your Device</p>
+                </div>
+              </div>
+              {fileUrl && (
+                <aside>
+                  <img src={fileUrl} alt="asset_file" />
+                </aside>
+              )}
+            </div>
           </div>
         </div>
       </div>
